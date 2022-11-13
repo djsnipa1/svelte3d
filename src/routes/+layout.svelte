0eda487b6d4e8d00c3c440dbddc8ae646b134beb
AXON <axonasif@gmail.com>
2022-11-13 22:02:12 +0000

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<div class="scene">
+	<slot />
+</div>
+
+<style>
+	.scene {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		inset: 0;
+		background-image: linear-gradient(
+			to right bottom,
+			#c6ff00,
+			#d7f200,
+			#e4e500,
+			#efd700,
+			#f7ca00,
+			#fdb50d,
+			#ff9f1e,
+			#ff8a2d,
+			#ff6549,
+			#ff3272,
+			#ff00a5,
+			#ff00df
+		);
+		background-attachment: fixed;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import * as Threlte from '@threlte/core';
+	import * as Three from 'three';
+	import * as MathUtils from 'three/src/math/MathUtils';
+</script>
+
+<Threlte.Canvas>
+	<Threlte.PerspectiveCamera position={{ x: 20, y: 20, z: 20 }} fov={50}>
+		<Threlte.OrbitControls autoRotate />
+	</Threlte.PerspectiveCamera>
+
+	<Threlte.AmbientLight color="white" intensity={0.2} />
+
+	<Threlte.DirectionalLight color="0xffffff" intensity={2} position={{ x: 10, y: 20 }} />
+
+	<Threlte.Mesh
+		geometry={new Three.SphereGeometry(4, 64, 64)}
+		material={new Three.MeshStandardMaterial({ color: '#fff' })}
+	/>
+
+	<Threlte.Mesh
+		geometry={new Three.PlaneGeometry(20, 20)}
+		material={new Three.MeshStandardMaterial({ color: '0xff00ff' })}
+	/>
+</Threlte.Canvas>
